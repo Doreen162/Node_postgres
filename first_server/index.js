6cbaac193c5651bb7a43dcde1b9f7c801adc6a7d
Doreen162 <doreenligman17@gmail.com>
2021-04-16 09:50:17 +0200

    ES6 in Node

--- first_server/index.js
+++ first_server/index.js
@@ -3,10 +3,11 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req,  res) =>{
+const server = http.createServer(/*function */(req,  res) => {
     res.end('Welcome to Node!');
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at ${hostname}: ${port}`);
+    //console.log("Server running at "+ hostname + ":" + port);
 });
